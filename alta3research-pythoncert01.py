86716760a5714940023d3e00772636f9c460c55c
Roger Shriner <rshriner@users.noreply.github.com>
2021-10-23 06:28:00 +0000

    touching up code

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -39,14 +39,15 @@ def main():
         print(message)
         # prompt for user response
         quest = input("What question do you have for the Magic PyBall?\n> ")
-        # create a random index - subtract 1 to properly address lists
-        index = random.randint(1,10)-1
+        # create a random index - subtract 2 to properly address lists and avoid goodbye message
+        index = random.randint(0,len(response)-2)
         # store random response to ans
         ans = response[index]
         # print response and continue or quit
         print("\nMagic PyBall says...")
         if quest == "QUIT":
-            ans = response[10]
+            # the last response is always the goodbye message
+            ans = response[-1]
             print(f"{ans}\n")
             # break out of loop and end game
             break
@@ -55,5 +56,6 @@ def main():
             # pause the screen for the user to read 
             input("Press ENTER to try again")
 
+# only run main() when we are running from this program... this isn't really necessary unless we have additional functions in this script
 if __name__ == "__main__":
     main()
